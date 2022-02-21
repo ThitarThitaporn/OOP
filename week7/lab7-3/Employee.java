7cefc2e8d38bc47c9767a1bed1ad46ccf686d0bf
ThitarThitaporn <64011100@KMITL.AC.TH>
2022-02-21 17:33:41 +0700

    lab7

---
+++ week7/lab7-3/Employee.java
@@ -0,0 +1,24 @@
+import java.time.LocalDate;
+
+public class Employee 
+    extends Person {
+
+    // Attributes
+    public String office;
+    public double salary;
+    public LocalDate dateHired;
+
+    // Constructors
+    public Employee() {
+
+    }
+
+    // Methods
+    @Override
+    public String toString() {
+        return super.toString() + "Employee{office=" + this.office
+                + ", salary=" + this.salary
+                + ", dateHired=" + this.dateHired
+                + "} ";
+    }
+}
