069c33ca7e3178ee3b20c6a865d11222634ca7b9
ThitarThitaporn <64011100@KMITL.AC.TH>
2022-04-04 16:45:43 +0700

    0

--- week10/IllegalTriangleException.java
+++
@@ -1,5 +0,0 @@
-public class IllegalTriangleException extends Exception {
-	public IllegalTriangleException(double side1, double side2, double side3) {
-		super("Invalid sides: side1 = " + side1 + ", side2 = " + side2 + ", side3 = " + side3);
-	}
-}
